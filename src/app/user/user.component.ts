8641f99859d0e48f3f8441609358e99cb08724b5
Apondi Otieno <apondotii@gmail.com>
2022-02-13 19:28:36 +0300

    this will add user infor

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,8 @@
+
+import { UserService } from './../user.service';
+import { User } from './../user';
 import { Component, OnInit } from '@angular/core';
+import { Repo } from '../repo';
 
 @Component({
   selector: 'app-user',
@@ -7,9 +11,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserComponent implements OnInit {
 
-  constructor() { }
+  user!: User;
+repo: Repo;
 
-  ngOnInit(): void {
+constructor(public myService: UserService, private repoService: UserService) {}
+
+searchs(searchName: string) {
+  this.myService.searchUser(searchName).then(
+    (success: any) => {
+      this.user = this.myService.getUser;
+    },
+    (error: any) => {
+      console.log(error);
+    }
+  );
+  this.repoService.getRepos(searchName).then(
+      (results: any) => {
+        this.repo = this.repoService.getRepo;
+        console.log(this.repo);
+      },
+      (error: any) => {
+        console.log(error);
+      }
+    );
+
+}
+ngOnInit() {
+this.searchs('Apondi-Otieno');
   }
 
 }
