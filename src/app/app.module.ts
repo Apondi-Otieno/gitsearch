424037790d0d570954f76c9a707d590e86fa7f97
Apondi Otieno <apondotii@gmail.com>
2022-02-14 07:31:58 +0300

    starting afresh

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomePageComponent } from './repo/home-page.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { FormComponent } from './form/form.component';
-import { UserComponent } from './user/user.component';
 import { NotfoundComponent } from './notfound/notfound.component';
-import { RepoComponent } from './repo/repo.component';
-import { User } from './user.service/user.service.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomePageComponent,
-    NavbarComponent,
-    FormComponent,
-    UserComponent,
     NotfoundComponent,
-    RepoComponent,
-    User.ServiceComponent
+
   ],
   imports: [
     BrowserModule,
--- src/app/date.pipe.spec.ts
+++
@@ -1,7 +0,0 @@
-import { Date.Pipe } from './date.pipe';
-
-describe('Date.Pipe', () => {
-  it('should create an instance', () => {
-    expect(new Date.Pipe()).toBeTruthy();
-  });
-});
