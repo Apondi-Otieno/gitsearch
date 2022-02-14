4bbff90bbe22465d8ca0eb89fd55ebb390bc8106
Apondi Otieno <apondotii@gmail.com>
2022-02-14 07:36:20 +0300

    this will add form component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NotfoundComponent } from './notfound/notfound.component';
+import { SearchFormComponent } from './search-form/search-form.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     NotfoundComponent,
+    SearchFormComponent,
 
   ],
   imports: [
